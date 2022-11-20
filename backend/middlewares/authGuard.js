d7470a1ba9590c4fe737fdd3b8468fffb88dd63d
Bruno Alves <bruno6821@gmail.com>
2022-11-20 16:37:53 -0300

    Implementação do Middleware de Autenticação do usuário

---
+++ backend/middlewares/authGuard.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+
+const authGuard = async (req, res, next) => {
+  // Verifica se o header é de Autorização
+  const authHeader = req.headers["authorization"];
+  // Quebra o header e pega o token
+  const token = authHeader && authHeader.split(" ")[1];
+  // Verifica se o Header tem um Token
+  if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
+  // Verifica se o Token é válido
+  try {
+    const verified = jwt.verify(token, jwtSecret);
+    // Tenta Recuperar o usuário do Token
+    req.user = await User.findById(verified.id).select("-password");
+    // Continua
+    next();
+  } catch (error) {
+    res.status(401).json({ errors: ["Acesso negado!"] });
+  }
+};
+
+module.exports = authGuard;
