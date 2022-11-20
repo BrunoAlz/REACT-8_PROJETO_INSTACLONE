fbfabe821659c4308c0570497d3c0c87d1ad2653
Bruno Alves <bruno6821@gmail.com>
2022-11-20 11:54:15 -0300

    Criando o Schema Photo

---
+++ backend/models/Photo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Criando o SCHEMA de FOTOS, Serve como blueprint para o Model
+const PhotoSchema = new Schema(
+  {
+    image: String,
+    title: String,
+    likes: Array,
+    comments: Array,
+    userId: mongoose.ObjectId,
+    userName: String,
+  },
+  { timestamps: true }
+);
+
+// Criando o model no MongoDB
+const Photo = mongoose.model("Photo", PhotoSchema);
+module.exports = PhotoSchema;
