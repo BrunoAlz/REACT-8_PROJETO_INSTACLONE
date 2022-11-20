c64e088823cbfd67dc837d5c5076bb65d91a0817
Bruno Alves <bruno6821@gmail.com>
2022-11-20 12:13:26 -0300

    Configurando as Rotas para arquivos estaticos, bem como inicio da implementação das configs do BD

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Rotas
+// Configurando os CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Configurando o Diretório para o Upload das Imagens
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// Configurando a Conexão com o BD
+require("./config/db.js");
+
+// Configurando Rotas
 const router = require("./routes/Router.js");
 app.use(router);
 
