6e327e0b50e00a85862a171f114f1491341c92ee
Bruno Alves <bruno6821@gmail.com>
2022-11-20 12:36:20 -0300

    Ajustando as Dependencias e Criando a Conexao com o MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
 // Conexão com o Banco de Dados MongoDB
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ul4mvju.mongodb.net/?retryWrites=true&w=majority`
+    );
+
+    console.log("Conectou ao Banco!");
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn();
+
+module.exports = conn;
+//
