10ef2e06b3da462b51acf7b067a463fd8961a2db
Bruno Alves <bruno6821@gmail.com>
2022-11-20 16:01:15 -0300

    Implementando o Registro do usuario no Banco de Dados

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,7 +14,39 @@ const generateToken = (id) => {
 
 // Registrar e Logar o usuário
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  // Verifica se o usuário já existe
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(442).json({ erros: ["Por favor, utilize outro E-mail"] });
+    return;
+  }
+
+  // Encrypta a senha do usuário
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Cria o usuário no Banco
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // Verifica se o usuário foi criado com sucesso, e retorna o Token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ erros: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 module.exports = {
