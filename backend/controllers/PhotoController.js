c6bf162c9bf80e6ca138c8ee8702733116c6a531
Bruno Alves <bruno6821@gmail.com>
2022-11-21 16:22:13 -0300

    Implementando a Search de Fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -172,6 +172,13 @@ const commentPhoto = async (req, res) => {
   });
 };
 
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+
+  res.status(200).json(photos);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -181,4 +188,5 @@ module.exports = {
   updatePhoto,
   likePhoto,
   commentPhoto,
+  searchPhotos,
 };
