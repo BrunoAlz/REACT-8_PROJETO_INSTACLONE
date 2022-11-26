406485dd7af2351732c5a560b8f71c047532098c
Bruno Alves <bruno6821@gmail.com>
2022-11-26 05:32:43 -0300

    Implementando o JSX do Formulário de Registro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,31 @@
-import "./Auth.css"
+import "./Auth.css";
+
+// Components
+import { Link } from "react-router-dom";
+// Hooks
+import { useState, useEffect } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default Register
+  return (
+    <div>
+      <h2>InstaClone</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a Senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já é registrado ? <Link to="/login">Log in</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
