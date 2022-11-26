7022ce7b544ce9638504a3d3916a38db0c00d262
Bruno Alves <bruno6821@gmail.com>
2022-11-26 05:38:49 -0300

    Ajustes de CSS

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ import {
   BsHouseDoorFill,
   BsFillPersonFill,
   BsFillCameraFill,
+  BsCamera2
 } from "react-icons/bs";
 
 // Hooks
@@ -34,7 +35,7 @@ const Navbar = () => {
 
   return (
     <nav id="nav">
-      <Link to="/"><span>InstaClone</span></Link>
+      <Link to="/"><BsCamera2 /> <span>InstaClone</span></Link>
       <form id="search-form">
         <BsSearch />
         <input type="text" placeholder="Pesquisar..." />
