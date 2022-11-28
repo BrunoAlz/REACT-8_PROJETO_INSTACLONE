67e8949afe200a8fcdd41fd00c4b45362dc07c7e
Bruno Alves <bruno6821@gmail.com>
2022-11-28 12:53:33 -0300

    Implementando o update photo Slice

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -53,6 +53,25 @@ export const deleteUserPhoto = createAsyncThunk(
   }
 );
 
+// Update user photo
+export const updateUserPhoto = createAsyncThunk(
+  "photo/update",
+  async (photoData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.updateUserPhoto(
+      { title: photoData.title },
+      photoData.id,
+      token
+    );
+    // Check for erros
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -106,6 +125,29 @@ export const photoSlice = createSlice({
         state.message = action.payload.message;
       })
       .addCase(deleteUserPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
+      .addCase(updateUserPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(updateUserPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photos.map((photo) => {
+          if (photo._id === action.payload.photo._id) {
+            return (photo.title = action.payload.photo.title);
+          }
+          return photo;
+        });
+
+        state.message = action.payload.message;
+      })
+      .addCase(updateUserPhoto.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
         state.photo = {};
