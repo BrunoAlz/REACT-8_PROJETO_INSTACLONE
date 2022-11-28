6ff85d997e6fdf0d9b40f5c5f790e2cdcfc80e0c
Bruno Alves <bruno6821@gmail.com>
2022-11-28 00:50:45 -0300

    Iimplementação do Service updateProfile

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -13,8 +13,21 @@ const profile = async (data, token) => {
   } catch (error) {}
 };
 
+const updateProfile = async (data, token) => {
+  const config = requestConfig("PUT", data, token, true);
+
+  try {
+    const res = await fetch(`${endpoint}/users/`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {}
+};
+
 const userService = {
   profile,
+  updateProfile,
 };
 
 export default userService;
