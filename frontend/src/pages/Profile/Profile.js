2d51ad100011afc8e122aef1df974d25900bb453
Bruno Alves <bruno6821@gmail.com>
2022-11-28 01:29:46 -0300

    Inicio da Implementação do PROFILE do usuário

---
+++ frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import "./Profile.css";
+
+// Utils
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+// Components
+import Message from "../../components/Message";
+
+// Icons
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
