aefe42a6897642d877ddd19cb054ac55449fe4ee
Bruno Alves <bruno6821@gmail.com>
2022-11-28 02:27:16 -0300

    Finalizando a Implementação do photo Service

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -1,5 +1,21 @@
 import { endpoint, requestConfig } from "../utils/config";
 
-const photoService = {};
+//  Publish an user photo
+
+const publishPhoto = async (data, token) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(`${endpoint}/photos`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {}
+};
+
+const photoService = {
+  publishPhoto,
+};
 
 export default photoService;
